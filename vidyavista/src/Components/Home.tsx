826268549cd8df499b130345f0a0e4136d73e0be
shafique-m <shafique.mohammed@planview.com>
2023-12-12 14:27:10 +0530

    Init vidyavista with typescript and bootstrap template
    Initializing  VidyaVista Project

---
+++ vidyavista/src/Components/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div className="container mt-5">
+      <h1 className="display-4">Hello, Bootstrap with TypeScript!</h1>
+      <p className="lead">This is a simple React app using TypeScript and Bootstrap.</p>
+    </div>
+  );
+};
+
+export default Home;
